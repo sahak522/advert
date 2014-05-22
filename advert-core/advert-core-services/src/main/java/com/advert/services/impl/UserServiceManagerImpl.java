5bd2e6ce226fa0fc9d2e1582c9d13b326d5a277a
sahak522 <sahak.ordukhanyan@gmail.com>
2014-05-23 00:24:15 +0400

    fix of dao with rename should be renamed back

--- advert-core/advert-core-services/src/main/java/com/advert/services/impl/UserServiceManagerImpl.java
+++ advert-core/advert-core-services/src/main/java/com/advert/services/impl/UserServiceManagerImpl.java
@@ -1,7 +1,7 @@
 package com.advert.services.impl;
 
 import com.advert.models.User;
-import com.advert.repositories.UserDAO;
+import com.advert.repositories.UserDao1;
 import com.advert.services.UserServiceManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,19 +15,19 @@ import java.util.List;
 public class UserServiceManagerImpl implements UserServiceManager {
 
     @Autowired
-    private UserDAO userDAO;
+    private UserDao1 userDao1;
 
 
     // API
     public void create(final User entity) {
-        userDAO.create(entity);
+        userDao1.create(entity);
     }
 
     public User findOne(final long id) {
-        return userDAO.findOne(id);
+        return userDao1.findOne(id);
     }
 
     public List<User> findAll() {
-        return userDAO.findAll();
+        return userDao1.findAll();
     }
 }
