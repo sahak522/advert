73f7fa618fc505cf52907be2a027d6c370536b36
sahak522 <sahak.ordukhanyan@gmail.com>
2014-05-24 22:30:23 +0400

    Creating admin structure base pages

---
+++ advert-admin/advert-admin-web/src/main/java/com/advert/admin/web/pages/AdminBasePage.java
@@ -0,0 +1,20 @@
+package com.advert.admin.web.pages;
+
+import org.apache.wicket.markup.head.IHeaderResponse;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Created by sahak_000 on 24.05.2014.
+ */
+public abstract class AdminBasePage extends WebPage {
+
+
+    public AdminBasePage () {
+        super();
+    }
+
+    @Override
+    public void renderHead (IHeaderResponse response) {
+
+    }
+}
